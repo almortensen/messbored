1428547adec53137f2f6111709484ebdeb8f61b7
almortensen <143033800+almort08@users.noreply.github.com>
2025-04-04 11:32:02 -0400

    remove everything again

--- src/stickynote.js
+++
@@ -1,9 +0,0 @@
-export default function Stickynote({text,tags}){
-    return(
-    <div style={{width:"20vw",height:"20vh"}}>
-         <p>{text}</p>
-         <p>{tags}</p> 
-    </div>
-)
-    
-}
